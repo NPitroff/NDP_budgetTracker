e6ae6e3bb467593c8e776142dccd28034e139bcd
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-01-27 17:31:36 -0800

    Added paths and manifest

--- public/index.js
+++ public/index.js
@@ -1,5 +1,18 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((reg) => {
+        console.log('Service worker registered.', reg);
+      });
+  });
+}
+
+
+// const idb = require("./idb");
+
 let transactions = [];
 let myChart;
+
 
 fetch("/api/transaction")
   .then(response => {
