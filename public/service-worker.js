92c7fa965dd72c31693cbdc8de470fc00ad01029
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-01 15:52:54 -0800

    Changed the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,8 +1,9 @@
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/db.js",
   "/index.js",
-  "/favicon.ico",
+  "/manifest.webmanifest",
   "/styles.css",
   "/icons/icon-192x192.png",
   "/icons/icon-512x512.png",
@@ -12,16 +13,26 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", function (evt) {
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Your files were pre-cached successfully!");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
+// self.addEventListener("install", function (evt) {
+//   evt.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("Your files were pre-cached successfully!");
+//       return cache.addAll(FILES_TO_CACHE);
+//     })
+//   );
 
-  self.skipWaiting();
+//   // self.skipWaiting();
+// });
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .then(self.skipWaiting())
+  );
 });
+
 
 // activate
 self.addEventListener("activate", function (evt) {
