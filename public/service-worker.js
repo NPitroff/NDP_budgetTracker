1bd93adcf48303e3a8ee09130b76bf23ffefc5db
NPitroff <40879599+NPitroff@users.noreply.github.com>
2021-02-01 14:20:00 -0800

    Added an 'Install' command to the service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,55 +2,74 @@ var CACHE_NAME = "my-site-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 var urlsToCache = [
-    "/",
-    "/idb.js",
-    "/index.js",
-    "/manifest.json",
-    "/styles.css",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png"
-  ];
-
-  self.addEventListener("install", function(event) {
-    // Perform install steps
-    event.waitUntil(
-      caches.open(CACHE_NAME).then(function(cache) {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
-      })
-    );
-  });
-  self.addEventListener("fetch", function(event) {
-    // cache all get requests to /api routes
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
+  "/",
+  "/idb.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png",
+];
+//install service worker
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+//activate the service worker==============================
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+//fetch all files===================================
+self.addEventListener("fetch", function (event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
           return fetch(event.request)
-            .then(response => {
+            .then((response) => {
               // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(event.request.url, response.clone());
               }
               return response;
             })
-            .catch(err => {
+            .catch((err) => {
               // Network request failed, try to get it from the cache.
               return cache.match(event.request);
             });
-        }).catch(err => console.log(err))
-      );
-      return;
-    }
-    event.respondWith(
-      fetch(event.request).catch(function() {
-        return caches.match(event.request).then(function(response) {
-          if (response) {
-            return response;
-          } else if (event.request.headers.get("accept").includes("text/html")) {
-            // return the cached home page for all requests for html pages
-            return caches.match("/");
-          }
-        });
-      })
+        })
+        .catch((err) => console.log(err))
     );
-  });
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(function () {
+      return caches.match(event.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
